29026501c128d04dca88b78834409af98f07aa1c
Anthony Mahanna <43019056+aMahanna@users.noreply.github.com>
2025-01-10 07:59:05 -0500

    retriever improvements (#17)
    * retriever improvements
    
    * new: `collection_count`
    
    * new: `empty_result` object
    
    * remove: `raise` no longer required
    
    * set `LOGFLAG` to `True`
    
    * Removing config variable ARANGO_EMBED_DIMENSION, getting embed dimension automatically from the db
    
    * minor cleanup
    
    * whitespace
    
    * log cleanup
    
    ---------
    
    Co-authored-by: Ajay Kallepalli <ajay.r.kallepalli@gmail.com>

--- comps/retrievers/arango/langchain/retriever_arango.py
+++ comps/retrievers/arango/langchain/retriever_arango.py
@@ -7,24 +7,23 @@ from typing import Any, Optional, Union
 
 from arango import ArangoClient
 from config import (
-    ARANGO_COLLECTION_NAME,
     ARANGO_DB_NAME,
     ARANGO_DISTANCE_STRATEGY,
-    ARANGO_EMBEDDING_DIMENSION,
     ARANGO_EMBEDDING_FIELD,
+    ARANGO_GRAPH_NAME,
     ARANGO_NUM_CENTROIDS,
     ARANGO_PASSWORD,
     ARANGO_TEXT_FIELD,
-    ARANGO_TRAVERSAL_GRAPH_NAME,
+    ARANGO_TRAVERSAL_ENABLED,
     ARANGO_TRAVERSAL_MAX_DEPTH,
     ARANGO_URL,
     ARANGO_USE_APPROX_SEARCH,
     ARANGO_USERNAME,
-    TEI_EMBEDDING_ENDPOINT,
-    TEI_EMBED_MODEL,
     HUGGINGFACEHUB_API_TOKEN,
     OPENAI_API_KEY,
     OPENAI_EMBED_MODEL,
+    TEI_EMBED_MODEL,
+    TEI_EMBEDDING_ENDPOINT,
 )
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings, HuggingFaceHubEmbeddings
 from langchain_community.vectorstores.arangodb_vector import ArangoVector
@@ -48,17 +47,16 @@ from comps.cores.proto.api_protocol import (
     RetrievalResponseData,
 )
 
+# TODO: Revisit these classes. How would they be presented in ChatQnA?
+# class ArangoTextDoc(TextDoc):
+#     neighborhood: Optional[list[dict[str, Any]]] = None
 
-class ArangoTextDoc(TextDoc):
-    neighborhood: Optional[list[dict[str, Any]]] = None
-
-
-class ArangoRetrievalResponseData(RetrievalResponseData):
-    neighborhood: Optional[list[dict[str, Any]]] = None
+# class ArangoRetrievalResponseData(RetrievalResponseData):
+#     neighborhood: Optional[list[dict[str, Any]]] = None
 
 
 logger = CustomLogger("retriever_arango")
-logflag = os.getenv("LOGFLAG", False)
+logflag = os.getenv("LOGFLAG", True)
 
 
 def fetch_neighborhoods(
@@ -70,20 +68,6 @@ def fetch_neighborhoods(
     max_depth: int,
 ) -> None:
     """Fetch neighborhoods of source documents. Updates the neighborhoods dictionary in-place."""
-    if not vector_db.db.has_graph(graph_name):
-        logger.error("Graph not found in database.")
-        return
-
-    graph = vector_db.db.graph(graph_name)
-
-    if not graph.has_edge_collection(f"{graph_name}_HAS_SOURCE"):
-        logger.error(f"Edge collection '{graph_name}_HAS_SOURCE' not found in graph.")
-        return
-
-    if not graph.has_edge_collection(f"{graph_name}_LINKS_TO"):
-        logger.error(f"Edge collection '{graph_name}_LINKS_TO' not found in graph.")
-        return
-
     if max_depth < 1:
         max_depth = 1
 
@@ -111,6 +95,9 @@ def fetch_neighborhoods(
     for doc in cursor:
         neighborhoods.update(doc)
 
+    if logflag:
+        logger.info(f"Fetched neighborhoods for {len(neighborhoods)} documents.")
+
 
 @register_microservice(
     name="opea_service@retriever_arango",
@@ -126,94 +113,214 @@ async def retrieve(
     if logflag:
         logger.info(input)
 
+    if isinstance(input, EmbedDoc):
+        empty_result = SearchedDoc(retrieved_docs=[], initial_query=input.text)
+    elif isinstance(input, RetrievalRequest):
+        empty_result = RetrievalResponse(retrieved_docs=[])
+    elif isinstance(input, ChatCompletionRequest):
+        input.retrieved_docs = []
+        input.documents = []
+        empty_result = input
+    else:
+        raise ValueError("Invalid input type: ", type(input))
+
     start = time.time()
 
     query = input.text if isinstance(input, EmbedDoc) else input.input
     embedding = input.embedding if isinstance(input.embedding, list) else None
 
+    ########################
+    # Fetch the Graph Name #
+    ########################
+
+    # This is a workaround as the ChatQnA UI is limited to
+    # a single input field, so we need to parse the graph name from the query (for now).
+
+    graph_name = None
+    query_split = query.split("|")
+
+    if len(query_split) == 2:
+        # e.g "Who is connected to John Smith? | PersonGraph"
+        query = query_split[0].strip()
+        graph_name = query_split[1].strip()
+
+    if not graph_name:
+        graph_name = ARANGO_GRAPH_NAME
+
+    source_collection_name = f"{graph_name}_SOURCE"
+
+    if not db.has_graph(graph_name):
+        if logflag:
+            graph_names = [g["name"] for g in db.graphs()]
+            logger.error(f"Graph '{graph_name}' does not exist in ArangoDB. Graphs: {graph_names}")
+
+        return empty_result
+
+    if not db.graph(graph_name).has_vertex_collection(source_collection_name):
+        if logflag:
+            collection_names = db.graph(graph_name).vertex_collections()
+            m = f"Collection '{source_collection_name}' does not exist in graph '{graph_name}'. Collections: {collection_names}"
+            logger.error(m)
+
+        return empty_result
+
+    collection = db.collection(source_collection_name)
+
+    collection_count = collection.count()
+    if collection_count == 0:
+        if logflag:
+            logger.error(f"Collection '{source_collection_name}' is empty.")
+
+        return empty_result
+
+    if collection_count < ARANGO_NUM_CENTROIDS:
+        if logflag:
+            m = f"Collection '{source_collection_name}' has fewer documents ({collection_count}) than the number of centroids ({ARANGO_NUM_CENTROIDS})."
+            logger.error(m)
+
+        return empty_result
+
+    ################################
+    # Retrieve Embedding Dimension #
+    ################################
+
+    random_doc = collection.random()
+    random_doc_id = random_doc["_id"]
+
+    embedding = random_doc.get(ARANGO_EMBEDDING_FIELD)
+
+    if not embedding:
+        if logflag:
+            logger.error(f"Document '{random_doc_id}' is missing field '{ARANGO_EMBEDDING_FIELD}'.")
+
+        return empty_result
+
+    if not isinstance(embedding, list):
+        if logflag:
+            logger.error(f"Document '{random_doc_id}' has a non-list embedding field, found {type(embedding)}.")
+
+        return empty_result
+
+    dimension = len(embedding)
+
+    if dimension == 0:
+        if logflag:
+            logger.error(f"Document '{random_doc_id}' has an empty embedding field.")
+
+        return empty_result
+
+    if OPENAI_API_KEY and OPENAI_EMBED_MODEL:
+        # Use OpenAI embeddings
+        embeddings = OpenAIEmbeddings(model=OPENAI_EMBED_MODEL, dimensions=dimension)
+    elif TEI_EMBEDDING_ENDPOINT and HUGGINGFACEHUB_API_TOKEN:
+        # create embeddings using TEI endpoint service
+        embeddings = HuggingFaceHubEmbeddings(
+            model=TEI_EMBEDDING_ENDPOINT, huggingfacehub_api_token=HUGGINGFACEHUB_API_TOKEN
+        )
+    else:
+        # create embeddings using local embedding model
+        embeddings = HuggingFaceBgeEmbeddings(model_name=TEI_EMBED_MODEL)
+
+    ######################
+    # Compute Similarity #
+    ######################
+
     vector_db = ArangoVector(
         embedding=embeddings,
-        embedding_dimension=ARANGO_EMBEDDING_DIMENSION,
+        embedding_dimension=dimension,
         database=db,
-        collection_name=ARANGO_COLLECTION_NAME,
+        collection_name=source_collection_name,
         embedding_field=ARANGO_EMBEDDING_FIELD,
         text_field=ARANGO_TEXT_FIELD,
         distance_strategy=ARANGO_DISTANCE_STRATEGY,
         num_centroids=ARANGO_NUM_CENTROIDS,
     )
 
-    if input.search_type == "similarity_score_threshold":
-        docs_and_similarities = await vector_db.asimilarity_search_with_relevance_scores(
-            query=query,
-            embedding=embedding,
-            k=input.k,
-            score_threshold=input.score_threshold,
-            use_approx=ARANGO_USE_APPROX_SEARCH,
-        )
-        search_res = [doc for doc, _ in docs_and_similarities]
-    elif input.search_type == "mmr":
-        search_res = await vector_db.amax_marginal_relevance_search(
-            query=query,
-            embedding=embedding,
-            k=input.k,
-            fetch_k=input.fetch_k,
-            lambda_mult=input.lambda_mult,
-            use_approx=ARANGO_USE_APPROX_SEARCH,
-        )
-    else:
-        # Default to basic similarity search
-        search_res = await vector_db.asimilarity_search(
-            query=query,
-            embedding=embedding,
-            k=input.k,
-            use_approx=ARANGO_USE_APPROX_SEARCH,
-        )
+    try:
+        if input.search_type == "similarity_score_threshold":
+            docs_and_similarities = await vector_db.asimilarity_search_with_relevance_scores(
+                query=query,
+                embedding=embedding,
+                k=input.k,
+                score_threshold=input.score_threshold,
+                use_approx=ARANGO_USE_APPROX_SEARCH,
+            )
+            search_res = [doc for doc, _ in docs_and_similarities]
+        elif input.search_type == "mmr":
+            search_res = await vector_db.amax_marginal_relevance_search(
+                query=query,
+                embedding=embedding,
+                k=input.k,
+                fetch_k=input.fetch_k,
+                lambda_mult=input.lambda_mult,
+                use_approx=ARANGO_USE_APPROX_SEARCH,
+            )
+        else:
+            # Default to basic similarity search
+            search_res = await vector_db.asimilarity_search(
+                query=query,
+                embedding=embedding,
+                k=input.k,
+                use_approx=ARANGO_USE_APPROX_SEARCH,
+            )
+    except Exception as e:
+        if logflag:
+            logger.error(f"Error during similarity search: {e}")
+
+        return empty_result
+
+    if not search_res:
+        if logflag:
+            logger.info("No documents found.")
+
+        return empty_result
+
+    ########################################
+    # Traverse Source Documents (optional) #
+    ########################################
 
     neighborhoods = {}
-    if ARANGO_TRAVERSAL_GRAPH_NAME:
+    if ARANGO_TRAVERSAL_ENABLED:
         fetch_neighborhoods(
             vector_db,
             neighborhoods,
             [r.id for r in search_res],
-            ARANGO_TRAVERSAL_GRAPH_NAME,
-            ARANGO_COLLECTION_NAME,
+            graph_name,
             ARANGO_TRAVERSAL_MAX_DEPTH,
         )
 
-    # return different response format
-    retrieved_docs: Union[list[ArangoTextDoc], list[ArangoRetrievalResponseData]] = []
-    if isinstance(input, EmbedDoc):
-        for r in search_res:
-            retrieved_docs.append(
-                ArangoTextDoc(
-                    text=r.page_content,
-                    id=r.id,
-                    neighborhood=neighborhoods.get(r.id),
-                )
-            )
+    ####################
+    # Process Response #
+    ####################
 
+    search_res_tuples = []
+    for r in search_res:
+        page_content = r.page_content
+        neighborhood = neighborhoods.get(r.id)
+
+        text = page_content
+        if neighborhood:
+            text += f"\n--------\nDocument Neighborhood:\n{neighborhood}"
+
+        search_res_tuples.append((r.id, text, r.metadata))
+
+    retrieved_docs: Union[list[TextDoc], list[RetrievalResponseData]] = []
+    if isinstance(input, EmbedDoc):
+        retrieved_docs = [TextDoc(id=id, text=text) for id, text, _ in search_res_tuples]
         result = SearchedDoc(retrieved_docs=retrieved_docs, initial_query=input.text)
 
     else:
-        for r in search_res:
-            retrieved_docs.append(
-                ArangoRetrievalResponseData(
-                    text=r.page_content,
-                    id=r.id,
-                    metadata=r.metadata,
-                    neighborhood=neighborhoods.get(r.id),
-                )
-            )
+        retrieved_docs = [
+            RetrievalResponseData(id=id, text=text, metadata=metadata) for id, text, metadata in search_res_tuples
+        ]
 
         if isinstance(input, RetrievalRequest):
             result = RetrievalResponse(retrieved_docs=retrieved_docs)
 
-        elif isinstance(input, ChatCompletionRequest):
+        else:
             input.retrieved_docs = retrieved_docs
             input.documents = [doc.text for doc in retrieved_docs]
             result = input
-        else:
-            raise ValueError("Invalid input type: ", type(input))
 
     statistics_dict["opea_service@retriever_arango"].append_latency(time.time() - start, None)
 
@@ -224,19 +331,6 @@ async def retrieve(
 
 
 if __name__ == "__main__":
-
-    if not ARANGO_EMBEDDING_DIMENSION:
-        raise ValueError("EMBED_DIMENSION must specified in advance.")
-
-    if OPENAI_API_KEY and OPENAI_EMBED_MODEL:
-        # Use OpenAI embeddings
-        embeddings = OpenAIEmbeddings(model=OPENAI_EMBED_MODEL, dimensions=ARANGO_EMBEDDING_DIMENSION)
-    elif TEI_EMBEDDING_ENDPOINT and HUGGINGFACEHUB_API_TOKEN:
-        # create embeddings using TEI endpoint service
-        embeddings = HuggingFaceHubEmbeddings(model=TEI_EMBEDDING_ENDPOINT, huggingfacehub_api_token=HUGGINGFACEHUB_API_TOKEN)
-    else:
-        # create embeddings using local embedding model
-        embeddings = HuggingFaceBgeEmbeddings(model_name=TEI_EMBED_MODEL)
 
     client = ArangoClient(hosts=ARANGO_URL)
     sys_db = client.db(name="_system", username=ARANGO_USERNAME, password=ARANGO_PASSWORD, verify=True)
